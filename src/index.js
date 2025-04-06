f0a87cca27983926e20ceb744070e5919c5000e2
camila.moreira <camilammoreira1@gmail.com>
2025-04-06 14:09:00 -0300

    tooltip

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./ui/styles/style.css";
+import Tooltip from "./ui/tooltip";
+
+// create tooltip
+
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+
+tooltip.init();
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        devMiddleware: {
+            publicPath: '/assets/',
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
