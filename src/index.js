9192f9e99665fae8f6c3e34c0bdbb5b47a55d86f
camila.moreira <camilammoreira1@gmail.com>
2025-04-06 15:31:52 -0300

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./ui/styles/style.css";
 import Tooltip from "./ui/tooltip";
+import Dropdown from "./ui/dropdown";
 
 // create tooltip
-
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
-
 tooltip.init();
+
+// create dropdown
+document.querySelectorAll(".dropdown").forEach(element => {
+    const dropdown = new Dropdown(element);
+    dropdown.init();
+})
